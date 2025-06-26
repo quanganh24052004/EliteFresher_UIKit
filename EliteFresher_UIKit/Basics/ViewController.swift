53e5739bc18516bd075f269506611b62f35935a8
quanganh24052004 <anh.nguyenquang24052004@gmail.com>
2025-06-26 17:50:13 +0700

    Initial Commit

---
+++ EliteFresher_UIKit/Basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EliteFresher_UIKit
+//
+//  Created by iKame Elite Fresher 2025 on 6/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
